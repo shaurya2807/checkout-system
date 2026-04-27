62c67dff251aa7bc03697bcaab3b26c5b4166847
Sanidhya Srivastava <shaurya.sanidhya@gmail.com>
2026-04-27 18:55:22 +0530

    chore: initial Spring Boot project setup

---
+++ checkout-system/src/main/java/com/checkout/checkout_system/CheckoutSystemApplication.java
@@ -0,0 +1,13 @@
+package com.checkout.checkout_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutSystemApplication.class, args);
+	}
+
+}
---
+++ checkout-system/src/test/java/com/checkout/checkout_system/CheckoutSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkout.checkout_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
